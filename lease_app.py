29b3a714ce37a4063109909bdc54b2c10596293c
Kiel1mcc <KIEL.MCCLEARY@GMAIL.COM>
2025-06-07 11:13:38 -0400

    Update lease_app.py

--- lease_app.py
+++ lease_app.py
@@ -22,7 +22,12 @@ def main():
     tier = st.selectbox("Select Tier:", [f"Tier {i}" for i in range(1, 9)])
 
     # County dropdown
-    selected_county = st.selectbox("Select County:", county_df["Dropdown_Label"])
+    selected_county = st.selectbox(
+    "Select County:",
+    county_df["Dropdown_Label"],
+    index=county_df[county_df["County"] == "Marion"].index[0]
+)
+
     county_tax = county_df[county_df["Dropdown_Label"] == selected_county]["Tax Rate"].values[0] / 100
 
     money_down = st.number_input("Money Down ($)", value=0.0)
